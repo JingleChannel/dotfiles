41c44cb8015012b7c523f193611854ff5d1ebd63
Alexander Kaiser <elexd3v@gmail.com>
2018-02-09 07:40:43 +0100

    add gitignore; add alias; update zshrc

---
+++ alias.sh
@@ -0,0 +1,10 @@
+# Set personal aliases, overriding those provided by oh-my-zsh libs,
+# plugins, and themes.
+# For a full list of active aliases, run `alias`.
+alias oimg="open -a ImageOptim ."
+alias yb="yarn bootstrap"
+alias ys="yarn start"
+alias rm="trash"
+alias bd="brew doctor"
+alias bl="brew leaves"
+alias bcl="brew cask list"
