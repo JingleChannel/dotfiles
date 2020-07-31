73ce783840b978b7d6b7aeeeb959ee92663f7f02
Alexander Kaiser <alexander.kaiser1@hornbach.com>
2020-07-31 09:29:36 +0200

    Add rebase aliases

--- alias.sh
+++ alias.sh
@@ -1,15 +1,13 @@
 #!/bin/zsh
 
-# Set personal aliases, overriding those provided by oh-my-zsh libs,
-# plugins, and themes.
 alias buc="brew upgrade && brew cask upgrade && brew cleanup"
 alias gc-="git checkout -"
 alias gl="git pull --rebase"
 alias gcd="git checkout develop"
-# Install global packages with every node version your are using with nvm.
-alias nin="nvm install node --reinstall-packages-from=node"
-# Usage `nrp 9`
-alias nrp="nvm reinstall-packages"
+alias grd="git checkout develop && git pull --rebase && git checkout - && git rebase develop"
+alias grm="git checkout master && git pull --rebase && git checkout - && git rebase master"
+alias nin="nvm install node --reinstall-packages-from=node" # Install global packages with every node version your are using with nvm.
+alias nrp="nvm reinstall-packages" # Usage `nrp 9`
 alias oimg="open -a ImageOptim ."
 alias rm="trash"
 alias ys="yarn start"
