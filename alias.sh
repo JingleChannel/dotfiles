a1df04af2bad656230aa366b0c806889760246be
Alexander Kaiser <radiobasler@gmail.com>
2023-08-23 18:24:37 +0200

    remove obsolete alias

--- alias.sh
+++ alias.sh
@@ -3,19 +3,9 @@
 alias buc="brew update && brew upgrade && brew cleanup"
 alias up="softwareupdate --install --all && omz update && buc"
 
-alias gc-="git checkout -"
 alias gl="git pull --rebase"
 alias gs="git switch"
-alias gcd="git checkout develop"
-alias grd="git checkout develop && git pull --rebase && git checkout - && git rebase develop"
-alias grm="git checkout master && git pull --rebase && git checkout - && git rebase master"
-alias nin="nvm install node --reinstall-packages-from=node" # Install global packages with every node version your are using with nvm.
-alias nrp="nvm reinstall-packages" # Usage `nrp 9`
-alias oimg="open -a ImageOptim ."
 alias rm="trash"
 alias ys="yarn start"
 alias yt="yarn test"
 alias yw="yarn watch"
-alias wtfgit="git ls-files -v | grep -e '^[hsmrck]'"
-alias wtfbrew="brew list | xargs brew reinstall"
-alias stresstest="yes > /dev/null & yes > /dev/null & yes > /dev/null & yes > /dev/null & yes > /dev/null & yes > /dev/null & yes > /dev/null & yes > /dev/null"
