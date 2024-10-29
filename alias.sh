361a66c8df546eb11c7429cc93d1d5ec0ec34327
Alexander Kaiser <brisk.group.0x@icloud.com>
2024-10-29 18:35:09 +0100

    chore: remove software update

--- alias.sh
+++ alias.sh
@@ -1,7 +1,6 @@
 #!/bin/zsh
 
 alias buc="brew update && brew upgrade && brew cleanup"
-alias up="softwareupdate --install --all && omz update && buc"
 
 alias gl="git pull --rebase"
 alias rm="trash"
